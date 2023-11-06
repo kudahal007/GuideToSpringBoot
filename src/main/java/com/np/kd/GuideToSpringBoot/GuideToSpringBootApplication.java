b1ee562302c3829428dad49ec7901f04268c02a1
Kumar Dahal <kudahal007@gmail.com>
2023-11-06 11:24:36 -0500

    Initial spring boot application setup.

---
+++ src/main/java/com/np/kd/GuideToSpringBoot/GuideToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.np.kd.GuideToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideToSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/np/kd/GuideToSpringBoot/GuideToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np.kd.GuideToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
